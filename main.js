988c3d4b1a9bf3bb83b5b179381ef2f92d0d6db4
cowcow1 <117519206+cowcow22@users.noreply.github.com>
2023-04-04 00:15:34 +0700

    Update main.js

--- main.js
+++ main.js
@@ -2,15 +2,15 @@
 var uang = 0;
 var jangkrik = 100;
 var poin = jangkrik;
-var jumlahUang = document.getElementById('jumlahUang');
+var jumlahUang = document.getElementById("jumlahUang");
 updateUang();
 function updateUang() {
-  x = '<p> uang = ' + uang + '</p>';
+  x = "<p> Uang = " + uang + "</p>";
   jumlahUang.innerHTML = x;
 }
 function nambahPoint() {
   uang += poin;
-  x = '<p> uang = ' + uang + '</p>';
+  x = "<p> Uang = " + uang + "</p>";
   jumlahUang.innerHTML = x;
   console.log(uang);
 }
@@ -19,19 +19,31 @@ function nambahPoint() {
 // START LEVEL 0
 var levelAutoClick = 0;
 var hargaAutoClick = 500;
-var autoClick = document.getElementById('autoClick');
+var autoClick = document.getElementById("autoClick");
 function updateAutoClick() {
-  a = '<p> Auto-Clickers = ' + levelAutoClick + '</p>' + '<p> Cost : ' + hargaAutoClick + '</p>';
+  a =
+    "<p> Auto-Clickers = " +
+    levelAutoClick +
+    "</p>" +
+    "<p> Cost : " +
+    hargaAutoClick +
+    "</p>";
   autoClick.innerHTML = a;
 }
 updateAutoClick();
 
 var levelCabang = 0;
 var hargaCabang = 500;
-var cabang = document.getElementById('cabang');
+var cabang = document.getElementById("cabang");
 function updateCabang() {
   poin = jangkrik * (levelCabang + 1);
-  b = '<p> Cabang = ' + levelCabang + '</p>' + '<p> Cost : ' + hargaCabang + '</p>';
+  b =
+    "<p> Cabang = " +
+    levelCabang +
+    "</p>" +
+    "<p> Cost : " +
+    hargaCabang +
+    "</p>";
   cabang.innerHTML = b;
 }
 updateCabang();
@@ -39,7 +51,7 @@ updateCabang();
 
 // START AUTO CLICKER
 function beliAutoClicker() {
-  if (uang < hargaAutoClick) alert('UANG ANDA TIDAK CUKUP');
+  if (uang < hargaAutoClick) alert("UANG ANDA TIDAK CUKUP");
   else {
     uang -= hargaAutoClick;
     hargaAutoClick *= 2;
@@ -53,7 +65,7 @@ function beliAutoClicker() {
 
 // START CABANG
 function beliCabang() {
-  if (uang < hargaCabang) alert('UANG ANDA TIDAK CUKUP');
+  if (uang < hargaCabang) alert("UANG ANDA TIDAK CUKUP");
   else {
     uang -= hargaCabang;
     hargaCabang *= 2;
@@ -64,53 +76,86 @@ function beliCabang() {
 }
 // END CABANG
 
-document.getElementById('buttonJangkrikBiasa').disabled = false;
-document.getElementById('buttonJangkrikPremium').disabled = true;
-document.getElementById('buttonJangkrikRaja').disabled = true;
+document.getElementById("buttonJangkrikBiasa").disabled = false;
+document.getElementById("buttonJangkrikPremium").disabled = true;
+document.getElementById("buttonJangkrikRaja").disabled = true;
 
 // START JANGKRIK BIASA
 var hargaJangkrikBiasa = 10000;
 var jangkrikBiasa = 500;
 function beliJangkrikBiasa() {
-  if (uang < hargaJangkrikBiasa) alert('UANG ANDA TIDAK CUKUP');
+  if (uang < hargaJangkrikBiasa) alert("UANG ANDA TIDAK CUKUP");
   else {
     uang -= hargaJangkrikBiasa;
     jangkrik = jangkrikBiasa;
     updateUang();
     updateCabang();
-    document.getElementById('buttonJangkrikBiasa').disabled = true;
-    document.getElementById('buttonJangkrikPremium').disabled = false;
+    document.getElementById("buttonJangkrikBiasa").disabled = true;
+    document.getElementById("buttonJangkrikPremium").disabled = false;
   }
 }
 // END JANGKRIK BIASA
 
 // START JANGKRIK Premium
-var hargaJangkrikPremium = 20000;
+var hargaJangkrikPremium = 50000;
 var jangkrikPremium = 1000;
 function beliJangkrikPremium() {
-  if (uang < hargaJangkrikPremium) alert('UANG ANDA TIDAK CUKUP');
+  if (uang < hargaJangkrikPremium) alert("UANG ANDA TIDAK CUKUP");
   else {
     uang -= hargaJangkrikPremium;
     jangkrik = jangkrikPremium;
     updateUang();
     updateCabang();
-    document.getElementById('buttonJangkrikPremium').disabled = true;
-    document.getElementById('buttonJangkrikRaja').disabled = false;
+    document
+      .getElementById("pencetKodok")
+      .removeChild(document.getElementById("pencetKodok").firstElementChild);
+
+    var a = document.createElement("img");
+    a.src = "filependukungUTSLAB/2.png";
+    a.style = "width: 50%";
+    document.getElementById("pencetKodok").appendChild(a);
+    document.getElementById("buttonJangkrikPremium").disabled = true;
+    document.getElementById("buttonJangkrikRaja").disabled = false;
   }
 }
 // END JANGKRIK Premium
 
 // START JANGKRIK Raja
-var hargaJangkrikRaja = 30000;
+var hargaJangkrikRaja = 100000;
 var jangkrikRaja = 2000;
 function beliJangkrikRaja() {
-  if (uang < hargaJangkrikRaja) alert('UANG ANDA TIDAK CUKUP');
+  if (uang < hargaJangkrikRaja) alert("UANG ANDA TIDAK CUKUP");
   else {
     uang -= hargaJangkrikRaja;
     jangkrik = jangkrikRaja;
     updateUang();
     updateCabang();
-    document.getElementById('buttonJangkrikRaja').disabled = true;
+    document
+      .getElementById("pencetKodok")
+      .removeChild(document.getElementById("pencetKodok").firstElementChild);
+
+    var a = document.createElement("img");
+    a.src = "filependukungUTSLAB/3.png";
+    a.style = "width: 50%";
+    document.getElementById("pencetKodok").appendChild(a);
+    document.getElementById("buttonJangkrikRaja").disabled = true;
   }
 }
 // END JANGKRIK Raja
+
+// START KODOK CLICKED
+const jangkrikClicked = document.querySelector(".jangkrikClick");
+
+jangkrikClicked.addEventListener("click", function () {
+  this.classList.toggle("clicked");
+  setTimeout(() => {
+    this.classList.remove("clicked");
+  }, 100);
+});
+
+// END KODOK CLICKED
+
+// START AUDIO
+var audio = document.getElementById("audio");
+audio.volume = 0.2;
+// END AUDIO
